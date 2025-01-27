039ddbb38b793e2ea3310c6235f8ea069526c472
= <=>
2025-01-27 18:22:09 +0500

    added lombok

--- src/main/java/Entity/ExchangeRate.java
+++ src/main/java/Entity/ExchangeRate.java
@@ -2,11 +2,13 @@ package Entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ExchangeRate {
     private Integer id;
     private Currency baseCurrency;
